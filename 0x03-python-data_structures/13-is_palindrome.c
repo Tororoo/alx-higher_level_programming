25db9058fa4ca2a22e92cc668900a293428941f0
Tororoo <mrouwassi@gmail.com>
2023-12-05 10:03:27 -0800

    fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,31 @@
-#include "list.h"
+#include "lists.h"
 
 /**
- * palindrome - recursive palind or not
+ * is_palindrome - recursive palindrome or not
  * @head: head list
  * Return: 0 if it is not palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (aux_palind(head, *head);
+    if (head == NULL || *head == NULL)
+        return (1);
+    return (aux_palind(head, *head));
 }
+
 /**
- * aux_palind - funct to know if is palindrome
+ * aux_palind - function to check if it is palindrome
  * @head: head list
  * @end: end list
+ * Return: 0 if it is not palindrome, 1 if it is a palindrome
  */
 int aux_palind(listint_t **head, listint_t *end)
 {
-	if (end == NULL)
-		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end ->n)
-	{
-		*head = (*head)->next;
-		return (1);
-	}
-	return (0);
+    if (end == NULL)
+        return (1);
+    if (aux_palind(head, end->next) && (*head)->n == end->n)
+    {
+        *head = (*head)->next;
+        return (1);
+    }
+    return (0);
 }
